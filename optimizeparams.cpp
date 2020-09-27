05f669a7023b373b1a620bbd8e1c5077a554c8d5
Lasse Steinnes <lasse.steinnes93@gmail.com>
2020-09-27 18:23:15 +0200

    Created new class

---
+++ optimizeparams.cpp
@@ -0,0 +1,6 @@
+#include "optimizeparams.hpp"
+#include<iostream>
+#include<armadillo>
+
+using namespace std;
+using namespace arma
---
+++ optimizeparams.hpp
@@ -0,0 +1,16 @@
+#ifndef OptimizeParams_HPP
+#define OptimizeParams_HPP
+#include <fstream>
+#include <armadillo>
+
+class OptimizeParams{
+  protected:
+    vec m_rho, m_N
+
+  public: //only use void functions and rather just cout
+    void initialize()
+    void optimize_rho(int rho, vec )
+    void optimize_n()
+  };
+
+#endif
