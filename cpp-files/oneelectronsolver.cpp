d43fafddd5ed3230023b40c8de76effb48336685
Sidra Rashid <sidrarashid@sidras-air-1.home>
2020-09-30 19:31:00 +0200

    Moved cpp and hpp files to folder.

--- cpp-files/oneelectronsolver.cpp
+++ cpp-files/oneelectronsolver.cpp
@@ -1,8 +1,14 @@
 #include "jacobimethodsolver.hpp"
 
 //solving task d) with one electron
-void OneElectronSolver::init(int N){
-  initialize(N); //Initialize funksjon fra superklassen
-}
+void OneElectronSolver::init(int N, double rho_max){
+  initialize(N,rho_max); //Initialize funksjon fra superklassen
+  task = "d";
+  omega_r = 0;
 
-//flere funksjoner nedover her
+  double hh = h*h;
+  for (int i= 0; i < N; ++i){ //looping over inner points
+      m_rho(i) = (i+1)*h;
+      A(i,i) = 2/hh + m_rho(i)*m_rho(i); //diagonal elements
+    }
+}
